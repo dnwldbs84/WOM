797cd98648aa8a4b749d4ded291f478650ff901e
Wook <dnwldbs84@gmail.com>
2018-01-11 18:28:55 +0900

    w

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var util = require('./modules/public/util.js');
 
 var allowCORS = function(req, res, next) {
   res.header('Acess-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   (req.method === 'OPTIONS') ?
     res.send(200) :
