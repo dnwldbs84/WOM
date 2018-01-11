59c0b28074a37d6220ef1595f9f268f4a657920b
Wook <dnwldbs84@gmail.com>
2018-01-11 18:31:57 +0900

    w

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var userStatTable = csvJson.toObject(dataJson.userStatData, csvJsonOption);
 var util = require('./modules/public/util.js');
 
 var allowCORS = function(req, res, next) {
-  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   (req.method === 'OPTIONS') ?
@@ -33,6 +33,12 @@ var allowCORS = function(req, res, next) {
 };
 
 app.use(allowCORS);
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
