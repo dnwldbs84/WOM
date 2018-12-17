65451a92e58ec1911ca46b266d34a2a886c8737f
dnwldbs84 <31395554+dnwldbs84@users.noreply.github.com>
2018-12-17 17:52:05 +0900

    Update DB.js

--- modules/server/DB.js
+++ modules/server/DB.js
@@ -3,12 +3,13 @@ var mysql = require('mysql'),
     pool = null;
 
 var setting = {
-  socketPath : '/var/run/mysqld/mysqld.sock',
+  // socketPath : '/var/run/mysqld/mysqld.sock',
   connectionLimit: 10,
   host: '',
   user: '',
   password: '',
   database: 'WOM',
+  // database: 'test',
 
   dateStrings: 'date'
 };
